bbaacaf0d15a73bf583f127f2d7e39e175378bdc
sheezan-yo <khansheezan30@gmail.com>
2026-07-01 22:05:02 +0530

    bug fixes

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,12 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+    ],
+    credentials: true,
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
